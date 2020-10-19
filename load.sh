f412aa734886b62a2c0e6593b2e42dc23b35f53e
Allen Day <allenday@users.noreply.github.com>
2020-10-19 21:53:55 +0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,7 +1,8 @@
-bq load \
-  --quote='' \
-  --field_delimiter=tab \
-  --source_format=CSV \
-  --project_id=$PROJECT \
-  $DATASET.$TABLE \
-  "gs://books/ngrams/books/googlebooks-eng-all-1gram-20120701-*.gz"
+PROJECT=xxx
+DATASET=words
+BUCKET="gs://books/ngrams/books"
+CORPUS="eng-all"
+TABLE="${CORPUS//-/_}"
+(for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do 
+echo "bq load --quote='' --field_delimiter=tab --source_format=CSV --project_id=$PROJECT $DATASET.$TABLE '$BUCKET/googlebooks-$CORPUS-1gram-20120701-$i.gz'";
+done) | parallel -j 10 
